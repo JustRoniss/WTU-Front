f9566d1e59e3a7de4edc92e5f04fccd9e76f6b52
Josué Teobaldo <josueteobaldo220@hotmail.com>
2024-06-11 10:07:47 -0300

    commit

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,24 @@
 # Etapa de build
 FROM node:16 AS build
 
-# Definindo o diretório de trabalho
 WORKDIR /app
 
-# Copiando os arquivos de configuração do package.json
 COPY package*.json ./
 
-# Instalando as dependências
 RUN npm install
 
-# Copiando todos os arquivos do projeto
 COPY . .
 
-# Rodando o comando de build da aplicação
+ENV REACT_APP_BACKEND_URL=http://backend:8080
+
 RUN npm run build
 
-# Definindo a imagem base para a próxima etapa
 FROM nginx:alpine
 
-# Copiando os arquivos de build da aplicação para o diretório de HTML do Nginx
-COPY --from=build /app/build /usr/share/nginx/html
+COPY --from=build /app/dist /usr/share/nginx/html
+
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
-# Expondo a porta 80 (se necessário)
 EXPOSE 80
 
-# Comando para iniciar o Nginx
 CMD ["nginx", "-g", "daemon off;"]
--- src/components/admin/Event/CreateEvents.tsx
+++ src/components/admin/Event/CreateEvents.tsx
@@ -1,189 +1,186 @@
-import React, { useEffect, useState } from 'react';
-import { Form, Input, Button, DatePicker, Select, TimePicker, ConfigProvider } from 'antd';
-import moment from 'moment';
-import api from '../../../../axiosConfig';
-import './../../../styles/createEvents.css';
-import locale from 'antd/lib/locale/pt_BR';
-import { EventFormValues } from './../../../interfaces/EventFormValues';
-import { Unit } from './../../../interfaces/Unit';
-import { User } from './../../../interfaces/User';
-import { UnitDTO } from '../../../interfaces/dto/UnitDTO';
-import { UserDTO } from '../../../interfaces/dto/UserDTO';
+    import React, { useEffect, useState } from 'react';
+    import { Form, Input, Button, DatePicker, Select, TimePicker, ConfigProvider } from 'antd';
+    import moment from 'moment';
 
-moment.locale('pt-br');
+    import api from '../../../../axiosConfig';
+    import './../../../styles/createEvents.css';
+    import locale from 'antd/lib/locale/pt_BR';
+    import { EventFormValues } from './../../../interfaces/EventFormValues';
+    import { Unit } from './../../../interfaces/Unit';
+    import { User } from './../../../interfaces/User';
+    import { UnitDTO } from '../../../interfaces/dto/UnitDTO';
+    import { UserDTO } from '../../../interfaces/dto/UserDTO';
 
-const CreateEvents: React.FC = () => {
-    const [units, setUnits] = useState<Unit[]>([]);
-    const [users, setUsers] = useState<User[]>([]);
-    const [loadingUnits, setLoadingUnits] = useState<boolean>(true);
-    const [loadingUsers, setLoadingUsers] = useState<boolean>(true);
+    moment.locale('pt-br');
 
-    useEffect(() => {
-        const fetchUnits = async () => {
-            try {
-                const response = await api.get('/units/get-all');
-                setUnits(response.data);
-                setLoadingUnits(false);
-            } catch (error) {
-                console.error('Erro ao buscar unidades:', error);
-                setLoadingUnits(false);
-            }
+    const CreateEvents: React.FC = () => {
+        const [units, setUnits] = useState<Unit[]>([]);
+        const [users, setUsers] = useState<User[]>([]);
+        const [loadingUnits, setLoadingUnits] = useState<boolean>(true);
+        const [loadingUsers, setLoadingUsers] = useState<boolean>(true);
+
+        useEffect(() => {
+            const fetchUnits = async () => {
+                try {
+                    const response = await api.get('/units/get-all');
+                    setUnits(response.data);
+                    setLoadingUnits(false);
+                } catch (error) {
+                    console.error('Erro ao buscar unidades:', error);
+                    setLoadingUnits(false);
+                }
+            };
+
+            const fetchUsers = async () => {
+                try {
+                    const response = await api.get('/users/get-all');
+                    setUsers(response.data);
+                    setLoadingUsers(false);
+                } catch (error) {
+                    console.error('Erro ao buscar usuários:', error);
+                    setLoadingUsers(false);
+                }
+            };
+
+            fetchUnits();
+            fetchUsers();
+        }, []);
+
+        const onFinish = (values: EventFormValues) => {
+            const { startDate, startTime, endDate, endTime, title, description, unit, usersEmail, iframe } = values;
+
+            const startDateTime = moment.parseZone(`${startDate.format('YYYY-MM-DD')} ${startTime.format('HH:mm:ss')}`, 'America/Sao_Paulo');
+
+            const endDateTime = moment.parseZone(`${endDate.format('YYYY-MM-DD')} ${endTime.format('HH:mm:ss')}`, 'America/Sao_Paulo');
+
+            moment.locale('pt-br');
+
+            const selectedUnits: UnitDTO[] = units.filter(u => unit.includes(u.id.toString())).map(u => ({ id: u.id }));
+            const selectedUsers: UserDTO[] = users.filter(u => usersEmail.includes(u.email)).map(u => ({ email: u.email }));
+
+            const event = {
+                title,
+                description,
+                startDate: startDateTime.toISOString(),
+                endDate: endDateTime.toISOString(),
+                units: selectedUnits,
+                users: selectedUsers,
+                iframe,
+            };
+
+            api.post('/events/create', event)
+                .then(response => {
+                    console.log('Evento criado:', response.data);
+                    alert('Evento criado com sucesso!');
+                })
+                .catch(error => {
+                    console.error('Erro ao criar evento:', error);
+                    alert('Erro ao criar evento!');
+                });
         };
 
-        const fetchUsers = async () => {
-            try {
-                const response = await api.get('/users/get-all');
-                setUsers(response.data);
-                setLoadingUsers(false);
-            } catch (error) {
-                console.error('Erro ao buscar usuários:', error);
-                setLoadingUsers(false);
-            }
-        };
-
-        fetchUnits();
-        fetchUsers();
-    }, []);
-
-    const onFinish = (values: EventFormValues) => {
-        const { startDate, startTime, endDate, endTime, title, description, unit, usersEmail, iframe } = values;
-
-        const startDateTime = moment(startDate).set({
-            hour: startTime.hour(),
-            minute: startTime.minute(),
-        });
-
-        const endDateTime = moment(endDate).set({
-            hour: endTime.hour(),
-            minute: endTime.minute(),
-        });
-
-        const selectedUnits: UnitDTO[] = units.filter(u => unit.includes(u.id)).map(u => ({ id: u.id }));
-        const selectedUsers: UserDTO[] = users.filter(u => usersEmail.includes(u.email)).map(u => ({ email: u.email }));
-
-        const event = {
-            title,
-            description,
-            startDate: startDateTime.toISOString(),
-            endDate: endDateTime.toISOString(),
-            units: selectedUnits,
-            users: selectedUsers,
-            iframe,
-        };
-
-        api.post('/events/create', event)
-            .then(response => {
-                console.log('Evento criado:', response.data);
-                alert('Evento criado com sucesso!');
-            })
-            .catch(error => {
-                console.error('Erro ao criar evento:', error);
-                alert('Erro ao criar evento!');
-            });
+        return (
+            <ConfigProvider locale={locale}>
+                <div className='container'>
+                    <h1 className='title'>Criar eventos</h1>
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <Form onFinish={onFinish}>
+                            <Form.Item
+                                name="title"
+                                rules={[{ required: true, message: 'Por favor insira o título do evento.' }]}
+                            >
+                                <Input showCount maxLength={30} placeholder="Título do evento" style={{ width: 626 }} />
+                            </Form.Item>
+                            <Form.Item
+                                name="description"
+                                rules={[{ required: true, message: 'Por favor insira a descrição do evento.' }]}
+                            >
+                                <Input showCount maxLength={30} placeholder="Descrição do evento" style={{ width: 626 }} />
+                            </Form.Item>
+                            <p style={{ color: "rgba(0, 0, 0, 0.50)", textAlign: "center" }}>Data de inicio e fim do evento</p>
+                            <div className='input-group-horizontal'>
+                                <Form.Item
+                                    name="startDate"
+                                    rules={[{ required: true, message: 'Por favor selecione a data de início.' }]}
+                                >
+                                    <DatePicker placeholder='Data início' />
+                                </Form.Item>
+                                <Form.Item
+                                    name="startTime"
+                                    rules={[{ required: true, message: 'Por favor selecione a hora de início.' }]}
+                                >
+                                    <TimePicker placeholder='Hora início' />
+                                </Form.Item>
+                                <Form.Item
+                                    name="endDate"
+                                    rules={[{ required: true, message: 'Por favor selecione a data de término.' }]}
+                                >
+                                    <DatePicker placeholder='Data fim' />
+                                </Form.Item>
+                                <Form.Item
+                                    name="endTime"
+                                    rules={[{ required: true, message: 'Por favor selecione a hora de término.' }]}
+                                >
+                                    <TimePicker placeholder='Hora fim' />
+                                </Form.Item>
+                            </div>
+                            <div className='input-group-horizontal'>
+                                <Form.Item
+                                    name="unit"
+                                    rules={[{ required: false, message: '' }]}
+                                >
+                                    <Select
+                                        mode='multiple'
+                                        placeholder="Convidar unidades"
+                                        style={{ width: 308 }}
+                                        loading={loadingUnits}
+                                        options={units.map(unit => ({
+                                            label: unit.name,
+                                            value: unit.id.toString() // Convert to string
+                                        }))}
+                                    />
+                                </Form.Item>
+                                <Form.Item
+                                    name="usersEmail"
+                                    rules={[{ required: false, message: '' }]}
+                                >
+                                    <Select
+                                        mode='multiple'
+                                        placeholder="Convidar usuários"
+                                        style={{ width: 308 }}
+                                        showSearch
+                                        allowClear
+                                        loading={loadingUsers}
+                                        filterOption={(input, option) =>
+                                            option?.label
+                                                ? option.label.toLowerCase().includes(input.toLowerCase())
+                                                : false
+                                        }
+                                        options={users.map(user => ({
+                                            label: user.email,
+                                            value: user.email
+                                        }))}
+                                    />
+                                </Form.Item>
+                            </div>
+                            <Form.Item
+                                name="iframe"
+                                rules={[{ required: true, message: 'Por favor insira o iframe do streamyard.' }]}
+                            >
+                                <Input.TextArea placeholder="Iframe do streamyard" style={{ width: 626 }} />
+                            </Form.Item>
+                            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: 3 }}>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit">
+                                        Criar Evento
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </ConfigProvider>
+        );
     };
 
-    return (
-        <ConfigProvider locale={locale}>
-            <div className='container'>
-                <h1 className='title'>Criar eventos</h1>
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Form onFinish={onFinish}>
-                        <Form.Item
-                            name="title"
-                            rules={[{ required: true, message: 'Por favor insira o título do evento.' }]}
-                        >
-                            <Input showCount maxLength={30} placeholder="Título do evento" style={{ width: 626 }} />
-                        </Form.Item>
-                        <Form.Item
-                            name="description"
-                            rules={[{ required: true, message: 'Por favor insira a descrição do evento.' }]}
-                        >
-                            <Input showCount maxLength={30} placeholder="Descrição do evento" style={{ width: 626 }} />
-                        </Form.Item>
-                        <p style={{ color: "rgba(0, 0, 0, 0.50)", textAlign: "center" }}>Data de inicio e fim do evento</p>
-                        <div className='input-group-horizontal'>
-                            <Form.Item
-                                name="startDate"
-                                rules={[{ required: true, message: 'Por favor selecione a data de início.' }]}
-                            >
-                                <DatePicker placeholder='Data início' />
-                            </Form.Item>
-                            <Form.Item
-                                name="startTime"
-                                rules={[{ required: true, message: 'Por favor selecione a hora de início.' }]}
-                            >
-                                <TimePicker placeholder='Hora início' />
-                            </Form.Item>
-                            <Form.Item
-                                name="endDate"
-                                rules={[{ required: true, message: 'Por favor selecione a data de término.' }]}
-                            >
-                                <DatePicker placeholder='Data fim' />
-                            </Form.Item>
-                            <Form.Item
-                                name="endTime"
-                                rules={[{ required: true, message: 'Por favor selecione a hora de término.' }]}
-                            >
-                                <TimePicker placeholder='Hora fim' />
-                            </Form.Item>
-                        </div>
-                        <div className='input-group-horizontal'>
-                            <Form.Item
-                                name="unit"
-                                rules={[{ required: false, message: '' }]}
-                            >
-                                <Select
-                                    mode='multiple'
-                                    placeholder="Convidar unidades"
-                                    style={{ width: 308 }}
-                                    loading={loadingUnits}
-                                    options={units.map(unit => ({
-                                        label: unit.name,
-                                        value: unit.id
-                                    }))}
-                                />
-                            </Form.Item>
-                            <Form.Item
-                                name="usersEmail"
-                                rules={[{ required: false, message: '' }]}
-                            >
-                                <Select
-                                    mode='multiple'
-                                    placeholder="Convidar usuários"
-                                    style={{ width: 308 }}
-                                    showSearch
-                                    allowClear
-                                    loading={loadingUsers}
-                                    filterOption={(input, option) =>
-                                        option?.label
-                                            ? option.label.toLowerCase().includes(input.toLowerCase())
-                                            : false
-                                    }
-                                    options={users.map(user => ({
-                                        label: user.email,
-                                        value: user.email
-                                    }))}
-                                />
-                            </Form.Item>
-                        </div>
-                        <Form.Item
-                            name="iframe"
-                            rules={[{ required: true, message: 'Por favor insira o iframe do streamyard.' }]}
-                        >
-                            <Input.TextArea placeholder="Iframe do streamyard" style={{ width: 626 }} />
-                        </Form.Item>
-                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: 3 }}>
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit">
-                                    Criar Evento
-                                </Button>
-                            </Form.Item>
-                        </div>
-                    </Form>
-                </div>
-            </div>
-        </ConfigProvider>
-    );
-};
-
-export default CreateEvents;
+    export default CreateEvents;
